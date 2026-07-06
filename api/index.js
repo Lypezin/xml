3e6587bf663f559b5e1e2e079f6973d5deab0b6e
Lypezin <Foolype@gmail.com>
2026-07-05 23:11:49 -0300

    fix: downgrade archiver to v6.0.1 for CommonJS compatibility in Node environment

--- api/index.js
+++ api/index.js
@@ -1,11 +1,1 @@
-try {
-  module.exports = require('../server');
-} catch (err) {
-  module.exports = (req, res) => {
-    res.status(500).json({
-      error: "Erro na inicialização do servidor (Boot Crash)",
-      message: err.message,
-      stack: err.stack
-    });
-  };
-}
+module.exports = require('../server');
