1ee937cb448b67f60a15111f52c86da10a9dc405
Lypezin <Foolype@gmail.com>
2026-07-05 23:08:46 -0300

    debug: wrap api/index.js in try-catch to print boot errors

--- api/index.js
+++ api/index.js
@@ -1,1 +1,11 @@
-module.exports = require('../server');
+try {
+  module.exports = require('../server');
+} catch (err) {
+  module.exports = (req, res) => {
+    res.status(500).json({
+      error: "Erro na inicialização do servidor (Boot Crash)",
+      message: err.message,
+      stack: err.stack
+    });
+  };
+}
